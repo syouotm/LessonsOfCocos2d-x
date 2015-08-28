0e56e62492dee64191373e2d664889e23d7fce92
syouotm <zhangxiang@MacBook-Pro-Syou.local>
2015-08-28 17:05:58 +0800

    update

---
+++ L02UsingSprite/Classes/HelloWorldScene.cpp
@@ -0,0 +1,46 @@
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Sprite* s = Sprite::create("img.jpg");
+    s->setAnchorPoint(Vec2(0,0));
+    s->cocos2d::Node::setPosition(Director::getInstance()->getVisibleSize()/2);
+    addChild(s);
+    
+    return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
